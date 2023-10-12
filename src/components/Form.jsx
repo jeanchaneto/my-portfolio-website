75b78deb66de71c7f739fe02895858f99489a615
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-10-12 16:48:08 +0200

    Form validation/ UI feedback

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,36 +2,39 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const form = useRef();
-    const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        setLoading(true);
-    
-        emailjs
-          .sendForm(
-            process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-            process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-            form.current,
-            process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-          )
-          .then(
-            (result) => {
-              console.log(result.text);
-              e.target.reset();
-              setSubmitted(true);
-            },
-            (error) => {
-              console.log(error.text);
-              setError(error.text);
-            }
-          )
-          .finally(() => setLoading(false));
-      };
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          setSubmitted(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setError(error.text);
+        }
+      )
+      .finally(() => setLoading(false));
+  };
+
+  const handleClose = () => {
+    setSubmitted(false);
+  };
 
   return (
     <form
@@ -40,7 +43,7 @@ const Form = () => {
       ref={form}
       onSubmit={sendEmail}
     >
-      <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg border border-white/20 p-6 rounded-xl bg-gradient-to-tl from-cyan-700/10 to-transparent">
+      <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg border border-white/20 p-6 rounded-xl bg-gradient-to-tl from-cyan-700/10 to-transparent relative overflow-hidden">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div className="sm:col-span-2">
             <label
@@ -98,12 +101,38 @@ const Form = () => {
             </div>
           </div>
         </div>
+        {/* Confirmation message */}
+        {submitted && (
+          <div className="absolute inset-0  backdrop-blur-xl rounded-xl">
+            <div className=" w-full h-full rounded-xl flex-col flex justify-center items-center p-6">
+              <p className="">Thank you for your message.</p>
+              <p className="">I'll get back to you as soon as possible!</p>
+              <div
+                onClick={handleClose}
+                className="text-gray-100 font-semibold cursor-pointer w-12 h-12 flex items-center justify-center "
+              >
+                x
+              </div>
+            </div>
+          </div>
+        )}
         <div className="mt-8 flex justify-end">
+          {error && (
+            <p className=" text-red-400 text-sm mx-auto  self-center pr-6 ">
+              Oops something went wrong, try again or email directly at{" "}
+              <a
+                href="mailto:jeanchaneto@gmail.com"
+                className=" underline font-semibold"
+              >
+                jeanchaneto@gmail.com
+              </a>
+            </p>
+          )}
           <button
             type="submit"
-            className="rounded-xl border border-white/40 px-3.5 py-2.5 text-center text-sm text-gray-100 shadow-sm transition-color duration-300 hover:bg-white/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+            className="rounded-xl min-w-fit border border-white/40 px-3.5 py-2.5 text-center text-sm text-gray-100 shadow-sm transition-color duration-300 hover:bg-white/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
           >
-            Send message
+            {loading ? "Sending..." : "Send message"}
           </button>
         </div>
       </div>
