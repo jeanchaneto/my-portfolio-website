17facf77480932c19b5158eca9c136025a5d8289
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-10-19 21:27:40 +0200

    Contact page animation

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,7 @@
+import AnimatedText from "@/components/AnimatedText";
 import Form from "@/components/Form"
 import { EnvelopeIcon } from "@heroicons/react/24/outline"
-
+import { motion} from "framer-motion";
 
 const contact = () => {
   return (
@@ -8,14 +9,22 @@ const contact = () => {
         <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
           <div className="relative px-6 sm:pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
             <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-              <h1 className="text-3xl font-bold tracking-tight text-gray-100">
+              <AnimatedText el="h1" className="text-3xl font-bold tracking-tight text-gray-100">
               Contact
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
+              </AnimatedText>
+              <motion.p 
+              variants={{ hidden: {opacity:0, y:24}, visible: {opacity:1, y:0, transition: { delay: 0.3, duration: 0.5}}}}
+              initial="hidden"
+              animate="visible"
+              className="mt-6 text-lg leading-8 text-gray-300">
               Available for hire and eager to bring technical and creative skills to your team or project. Let's discuss how we can build something great together.
-              </p>
+              </motion.p>
               <div className="mt-10 space-y-4 text-base leading-7 text-gray-300">
-                <div className="flex gap-x-4">
+                <motion.div 
+                variants={{ hidden: {opacity:0, y:24}, visible: {opacity:1, y:0, transition: { delay: 0.5, duration: 0.5}}}}
+                initial="hidden"
+                animate="visible"
+                className="flex gap-x-4">
                   <div className="flex-none">
                     <span className="sr-only">Email</span>
                     <EnvelopeIcon
@@ -31,7 +40,7 @@ const contact = () => {
                       jeanchaneto@gmail.com
                     </a>
                   </div>
-                </div>
+                </motion.div>
               </div>
             </div>
           </div>
