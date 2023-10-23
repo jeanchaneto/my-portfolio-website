3289991a32d7d9d7fe0dd6903f9b258b7ad3b090
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-10-23 15:12:18 +0200

    Clean up files

--- src/components/Divider.jsx
+++
@@ -1,9 +0,0 @@
-export const Divider = () => {
-  return (
-    <div className="relative">
-      <div className="absolute inset-0 flex items-center px-6" aria-hidden="true">
-        <div className=" max-width-4xl w-[890px] mx-auto border-t border-white/10 " />
-      </div>
-    </div>
-  );
-};
